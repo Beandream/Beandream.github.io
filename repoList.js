a8297db2c099741adb54fc2578c5290a22f03b58
Bean Dream <beandreamed@gmail.com>
2020-10-07 22:24:10 -0400

    added better syling and another project
    added ludum dare game jame 42

--- repoList.js
+++ repoList.js
@@ -10,6 +10,12 @@ const list = [
         name: 'WallPaperBot',
         description: 'A Discord Bot made for organizing a Wallpaper Competition.',
         info: 'This is a Discord bot made originally for the AnxiousCynic Discord server where we hosted wallpaper competitions. This bot just organized the images for easier displaying when reveiwing and picking winners of the competition. This project required me to learn more about async data fetching, node.js running, and heroku hosting to run my discord bot.'
+    },
+    {
+        url: 'beandream.com/Ludum-Dare-42',
+        name: 'Ludum Dare 42',
+        description: 'A Game made for the Ludum Dare 42 game jam.',
+        info: 'A simple platformer game using Phaser 3. Killing slimes gives you enough gold to buy a rocket ship and escape the ever closing in spikes. Act Quickly!'
     }
 ]
 export {list}
